f039df3d0dd8b945721ecfeff2757080deff8c0e
Zeheng Li <imzehengl@gmail.com>
2021-01-12 09:31:40 -0700

    add prettier config

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,15 @@
   export let name;
 </script>
 
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
+
 <style>
   main {
     text-align: center;
@@ -23,12 +32,3 @@
     }
   }
 </style>
-
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-</main>
